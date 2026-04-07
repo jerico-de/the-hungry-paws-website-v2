dae42e8644e3ff1b512cfbf2f768c0fdce00b932
Jerico Crisostomo <jericocrisostomo29@gmail.com>
2026-04-07 11:09:50 +0800

    465 email port

--- src/utils/email.js
+++ src/utils/email.js
@@ -8,8 +8,8 @@ function getTransporter() {
 
   _transporter = nodemailer.createTransport({
     host:   process.env.EMAIL_HOST || "smtp.gmail.com",
-    port:   Number(process.env.EMAIL_PORT) || 587,
-    secure: false,
+    port:   Number(process.env.EMAIL_PORT) || 465,
+    secure: true,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
