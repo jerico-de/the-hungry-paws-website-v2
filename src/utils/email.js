5d078eeb8502bb3512371b2695fcdfdfaf37a668
Jerico Crisostomo <jericocrisostomo29@gmail.com>
2026-04-07 10:55:02 +0800

    resend config from

--- src/utils/email.js
+++ src/utils/email.js
@@ -2,7 +2,7 @@
 const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const FROM = "The Hungry Paws <onboarding@resend.dev>"; // replace with your domain later
+const FROM = "The Hungry Paws <onboarding@resend.dev>"; 
 
 async function safeSend({ to, subject, html }) {
   try {
